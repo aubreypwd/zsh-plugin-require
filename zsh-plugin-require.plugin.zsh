c8b302ef5b306c723c55b1e42e6ae9836e4be5c2
Aubrey Portwood <aubreypwd@users.noreply.github.com>
2020-10-01 07:56:01 -0600

    Add the function.

--- zsh-plugin-require.plugin.zsh
+++ zsh-plugin-require.plugin.zsh
@@ -1,7 +1,19 @@
 #!/bin/zsh
 
 ###
- # Require
+ # Require a command (and install if missing).
  #
+ # E.g: require "git" "brew reinstall git"
+ #
+ # @since Sun June 9th 2019
  # @since 1.0.0
  ##
+function require {
+	cmd="$1"
+	install="$2"
+
+	if ! [ -x "$(command -v $cmd)" ]; then
+		echo "Could not find $cmd, installing using: $install..."
+		eval ${install}
+	fi
+}
