0438df5c306042af6ea13df424254851856837f9
Aubrey Portwood <code@aubreypwd.com>
2020-10-01 15:05:42 -0600

    Add a 3rd option to specify package manager command to check for.

--- zsh-plugin-require.plugin.zsh
+++ zsh-plugin-require.plugin.zsh
@@ -5,12 +5,24 @@
  #
  # E.g: require "git" "brew reinstall git"
  #
+ #     This will simply run the command as the 2nd option.
+ #
+ # E.g: require "git" "brew reinstall git" "brew"
+ #
+ #     This will only run the command if a package manager as the 3rd option is present, e.g. `brew`.
+ #
  # @since Sun June 9th 2019
  # @since 1.0.0
  ##
 function require {
-	cmd="$1"
-	install="$2"
+	local cmd="$1"
+	local install="$2"
+	local manager="$3"
+
+	if ! [ -z $manager ] && ! [ -x "$(command -v $manager)" ]; then
+		echo "Package manger $manager not found, please install $cmd using your favorite package manager."
+		return
+	fi
 
 	if ! [ -x "$(command -v $cmd)" ]; then
 		echo "Could not find $cmd, installing using: $install..."
